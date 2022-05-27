2960e2aa5ea61fb61753c54c873b0db2982ceab6
Rachel Howell <rac777blue@hotmail.com>
2022-05-27 15:54:21 +0100

    add quote array to print random messages

--- pride-bot.rb
+++ pride-bot.rb
@@ -1,10 +1,28 @@
 require 'uri'
 require 'net/http'
 
-uri = URI('https://hooks.slack.com/services/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')
-https = Net::HTTP.new(uri.host,uri.port)
-https.use_ssl = true
-request = Net::HTTP::Post.new(uri.path)
-request.body = "{'text': 'Rachel is cool!'}"
-response = https.request(request)
-puts response
+$quoteArray = [
+  "\“Darling, I want my gay rights now. I think it’s about time the gay brothers and sisters got their rights… especially the women.\” ~Marsha P. Johnson",
+  "\"Nature made a mistake, which I have corrected.\" ~Christine Jorgensen",
+  "\"The richness, beauty and depths of love can only be fully experienced in a climate of complete openness, honesty and vulnerability\" ~Anthony Venn Brown",
+  "\Scratch a Transphobe, and you'll find a racist underneath\" ~Ashleigh Talbot"
+]
+
+def get_quote
+    puts $quoteArray.length
+    $quoteArray[rand($quoteArray.length)]
+end
+
+def message message_to_post
+  uri = URI('https://hooks.slack.com/services/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')
+  https = Net::HTTP.new(uri.host,uri.port)
+  puts https
+  https.use_ssl = true
+  request = Net::HTTP::Post.new(uri.path)
+  request.body = "{'text': '#{message_to_post}'}"
+  puts "{'text': '#{message_to_post}'}"
+  response = https.request(request)
+  puts response
+end
+
+puts message(get_quote)
